0288aaefcf12856c63511870552ab73886199fa8
pc-1310 <184345122+pc-1310@users.noreply.github.com>
2025-03-14 23:52:41 +0530

    Initial commit - project setup with .gitignore

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app=express();
+
+const PORT = process.env.PORT||3000;
+console.log(PORT);
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.get("/test",(req,res)=>{
+    res.send("Hello from test route");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}).on('error', (err) => {
+  console.error("Server error:", err);
+});
+
+
