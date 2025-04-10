a173d4e96654618cb28a60de97397c61ee7ba382
pc-1310 <184345122+pc-1310@users.noreply.github.com>
2025-04-10 21:53:06 +0530

    Added rate limiting and bot detection

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,32 @@ console.log(PORT);
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(morgan("dev"));
+app.use(morgan("dev")); //log the requests
+
+//apply arcjet rate-limit to all routes
+
+app.use(async(req,res,next)=>{
+  try{
+    const decision=await aj.project(req, {
+      requested: 1, // specifies that each request consumes one token
+    });
+    if(decision.isDenied()){
+      if(decision.reason.isRateLimit()) {
+        res.status(429).json({error: "Too Many Requests"});
+      } else if(decision.reason.isBot()) {
+        res.status(403).json({error: "Bot access denied"});
+      }else {
+        res.status(403).json({error: "Forbidden"});
+      }
+      return;
+    }
+    next();
+  } catch(error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
+
 app.use("/api/products",productsRoutes);
 
 async function initDB(){
