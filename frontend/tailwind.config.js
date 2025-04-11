aa491d2f9abc5e0a8733ed38fb4e57351b5b5693
pc-1310 <184345122+pc-1310@users.noreply.github.com>
2025-04-12 00:24:13 +0530

    frontend setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,49 @@
+
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+  // 👇 Move this inside plugin config if you're using Tailwind v3.3+
+  daisyui: {
+    themes: [
+      "pastel",
+      "retro",
+      "coffee",
+      "forest",
+      "cyberpunk",
+      "synthwave",
+      "luxury",
+      "autumn",
+      "valentine",
+      "aqua",
+      "business",
+      "night",
+      "dracula",
+    ],
+  },
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
